3c9bd6f1cb5eabd75a16a74c9ca738539c06018a
YunquanCheung <54952009+YunquanCheung@users.noreply.github.com>
2021-08-12 15:31:42 +0800

    Code Style Change

--- MenuView.java
+++ MenuView.java
@@ -18,95 +18,90 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MenuView extends JFrame {
+  JLabel nameLabel = new JLabel("Welcome to ConnectFour Game", JLabel.CENTER);
+  SpringLayout springlayout = new SpringLayout();
+  JPanel centerPanel = new JPanel(springlayout);
+  JButton pvpBtn = new JButton("INSTRUCTION, READ IT FIRST");
+  JButton pvcBtn = new JButton("PLAYER VS PLAYER");
+  JButton boardBtn = new JButton("PLAYER VS COMPUTER");
+  JButton backBtn = new JButton("EXIT");
+  private JMenu menu = new JMenu("Game");
+  MenuHandler menuHandler;
 
-    JLabel nameLabel = new JLabel("Welcome to ConnectFour Game",JLabel.CENTER);
-    SpringLayout springlayout = new SpringLayout();
-    JPanel centerPanel = new JPanel(springlayout);
-    JButton pvpBtn = new JButton("INSTRUCTION, READ IT FIRST");
-    JButton pvcBtn = new JButton("PLAYER VS PLAYER");
-    JButton boardBtn = new JButton("PLAYER VS COMPUTER");
-    JButton backBtn = new JButton("EXIT");
-    private JMenu menu = new JMenu("Game");
-    MenuHandler menuHandler;
+  public MenuView() {
+    super("ConnectFour");
+    menuHandler = new MenuHandler(this);
+    //Set label and buttons style
+    nameLabel.setFont(new Font("Brush Script Std", Font.PLAIN, 50));
+    nameLabel.setPreferredSize(new Dimension(0, 200));
+    //Set Style of words in buttons
+    Font cenFont = new Font("Times New Roman", Font.PLAIN, 16);
+    pvpBtn.setFont(cenFont);
+    pvcBtn.setFont(cenFont);
+    boardBtn.setFont(cenFont);
+    backBtn.setFont(cenFont);
 
-    public MenuView(){
+    //Add all the buttons into Panel
+    Dimension preferredSize = new Dimension(260, 60);    //set buttons' size
+    pvpBtn.setPreferredSize(preferredSize);    //set buttons' size
+    pvcBtn.setPreferredSize(preferredSize);
+    boardBtn.setPreferredSize(preferredSize);
+    backBtn.setPreferredSize(preferredSize);
 
-        super("ConnectFour");
-        menuHandler = new MenuHandler(this);
-        Container contentPane = getContentPane();
+    //Add all the buttons into the center Panel
+    pvpBtn.addActionListener(menuHandler);
+    centerPanel.add(pvpBtn);
 
-        //Set label and buttons style
-        nameLabel.setFont(new Font("Brush Script Std",Font.PLAIN,50));
-        nameLabel.setPreferredSize(new Dimension(0,200));
+    pvcBtn.addActionListener(menuHandler);
+    centerPanel.add(pvcBtn);
 
-        //Set Style of words in buttons
-        Font cenFont = new Font("Times New Roman", Font.PLAIN, 16);
-        pvpBtn.setFont(cenFont);
-        pvcBtn.setFont(cenFont);
-        boardBtn.setFont(cenFont);
-        backBtn.setFont(cenFont);
+    boardBtn.addActionListener(menuHandler);
+    centerPanel.add(boardBtn);
 
-        //Add all the buttons into Panel
-        Dimension preferredSize=new Dimension(260, 60);    //set buttons' size
-        pvpBtn.setPreferredSize(preferredSize);    //set buttons' size
-        pvcBtn.setPreferredSize(preferredSize);
-        boardBtn.setPreferredSize(preferredSize);
-        backBtn.setPreferredSize(preferredSize);
+    backBtn.addActionListener(menuHandler);
+    centerPanel.add(backBtn);
 
-        //Add all the buttons into the center Panel
-        pvpBtn.addActionListener(menuHandler);
-        centerPanel.add(pvpBtn);
+    //Spring layout code
+    layoutCenter();
+    //board layout code
+    Container contentPane = getContentPane();
+    contentPane.add(nameLabel, BorderLayout.NORTH);
+    contentPane.add(centerPanel, BorderLayout.CENTER);
+    //windows' setting
+    this.setBounds(0, 0, 738, 685);
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    this.setResizable(false);
+    this.setVisible(true);
+    this.setLayout(null);
+  }
 
-        pvcBtn.addActionListener(menuHandler);
-        centerPanel.add(pvcBtn);
+  //This method does nothing other than Layout settings.
+  private void layoutCenter() {
+    //Spring Layout
+    Spring width = Spring.width(pvpBtn);
+    int offsetX = width.getValue() / 2;
 
-        boardBtn.addActionListener(menuHandler);
-        centerPanel.add(boardBtn);
+    //Layout pvp Button
+    springlayout.putConstraint(SpringLayout.WEST, pvpBtn, -offsetX,
+            SpringLayout.HORIZONTAL_CENTER, centerPanel);
+    springlayout.putConstraint(SpringLayout.NORTH, pvpBtn, 30,
+            SpringLayout.NORTH, centerPanel);
 
-        backBtn.addActionListener(menuHandler);
-        centerPanel.add(backBtn);
-
-        //Spring layout code
-        LayoutCenter();
-        //board layout code
-        contentPane.add(nameLabel,BorderLayout.NORTH);
-
-        contentPane.add(centerPanel,BorderLayout.CENTER);
-        //windows' setting
-        this.setBounds(0, 0, 738, 685);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.setVisible(true);
-        this.setLayout(null);
-    }
-
-    //This method does nothing other than Layout settings.
-    private void LayoutCenter() {
-        //Spring Layout
-        Spring Width = Spring.width(pvpBtn);
-        int offsetX = Width.getValue() / 2;
-
-        //Layout pvp Button
-        springlayout.putConstraint(SpringLayout.WEST, pvpBtn, -offsetX,
-                SpringLayout.HORIZONTAL_CENTER, centerPanel);
-        springlayout.putConstraint(SpringLayout.NORTH, pvpBtn, 30,
-                SpringLayout.NORTH, centerPanel);
-
-        // pvc button
-        springlayout.putConstraint(SpringLayout.WEST, pvcBtn, -offsetX,
-                SpringLayout.HORIZONTAL_CENTER, centerPanel);
-        springlayout.putConstraint(SpringLayout.NORTH, pvcBtn, 30,
-                SpringLayout.SOUTH, pvpBtn);
-        // board button
-        springlayout.putConstraint(SpringLayout.WEST, boardBtn, -offsetX,
-                SpringLayout.HORIZONTAL_CENTER, centerPanel);
-        springlayout.putConstraint(SpringLayout.NORTH, boardBtn, 30,
-                SpringLayout.SOUTH, pvcBtn);
-        // board button
-        springlayout.putConstraint(SpringLayout.WEST, backBtn, -offsetX,
-                SpringLayout.HORIZONTAL_CENTER, centerPanel);
-        springlayout.putConstraint(SpringLayout.NORTH, backBtn, 30,
-                SpringLayout.SOUTH, boardBtn);
-    }
+    // pvc button
+    springlayout.putConstraint(SpringLayout.WEST, pvcBtn, -offsetX,
+            SpringLayout.HORIZONTAL_CENTER, centerPanel);
+    springlayout.putConstraint(SpringLayout.NORTH, pvcBtn, 30,
+            SpringLayout.SOUTH, pvpBtn);
+    // board button
+    springlayout.putConstraint(SpringLayout.WEST, boardBtn, -offsetX,
+            SpringLayout.HORIZONTAL_CENTER, centerPanel);
+    springlayout.putConstraint(SpringLayout.NORTH, boardBtn, 30,
+            SpringLayout.SOUTH, pvcBtn);
+    // board button
+    springlayout.putConstraint(SpringLayout.WEST, backBtn, -offsetX,
+            SpringLayout.HORIZONTAL_CENTER, centerPanel);
+    springlayout.putConstraint(SpringLayout.NORTH, backBtn, 30,
+            SpringLayout.SOUTH, boardBtn);
+  }
 
 }
